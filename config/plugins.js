db2932d00e9d0857c4d1acb67b443282a43bde73
jcharry <jamie.charry@gmail.com>
2016-06-12 20:40:10 -0400

    refactor to run webpack on heroku

---
+++ config/plugins.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var configure = () => {
+    var shared = [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './app/index.html'
+        }),
+    ]
+    if (process.env.NODE_ENV === 'production') {
+        return shared.concat([
+            new webpack.optimize.UglifyJsPlugin({
+                compressor: {
+                    warnings: false,
+                },
+            }),
+            new webpack.optimize.OccurenceOrderPlugin()
+        ]);
+    } else {
+        return shared;
+    }
+}
+
+module.exports = configure();
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack')
+var path = require('path')
+var loaders = require('./config/loaders');
+var plugins = require('./config/plugins');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+console.log(plugins);
+
+module.exports = {
+    devtool: process.env.NODE_ENV === 'production' ? null : 'cheap-eval-source-map',
+    entry: process.env.NODE_ENV === 'production' ? 
+        './app/app.js' : [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/dev-server',
+        './app/app.js'
+    ],
+    output: { 
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+        //publicPath: 'dist/'
+    },
+    resolve: {
+        extensions: ['', '.js'],
+        alias: {
+            'webworkify': 'webworkify-webpack'
+        }
+    },
+    module: loaders,
+    plugins: plugins,
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    }
+};
