bc023f62f6fe7223ae50002b37697ef9ed5778e7
jcharry <jamie.charry@gmail.com>
2016-06-12 20:45:08 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     },
     module: loaders,
     plugins: plugins,
-    devServer: {
+    devServer: process.env.NODE_ENV === 'production' ? null : {
         contentBase: './dist',
         hot: true
     }
