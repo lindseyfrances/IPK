0877e8c80a3e30b6596aba36b7c5883773bb495d
jcharry <jamie.charry@gmail.com>
2016-07-27 02:03:55 -0400

    set up env vars

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -4,7 +4,7 @@ import { TextDecoder } from 'text-encoding';
 import creds from 'app/creds/creds';
 
 var AWS = require('aws-sdk');
-AWS.config.update({accessKeyId: creds.AWS_ACCESS_KEY, secretAccessKey: creds.AWS_SECRET_KEY});
+AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY, secretAccessKey: process.env.AWS_SECRET_KEY});
 var s3 = new AWS.S3();
 
 export const startLoading = () => {
